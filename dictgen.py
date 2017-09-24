d0b7423b64f71705d5efc20401ee18c74b04fbbb
David Haynes <dhaynes3@gmu.edu>
2017-09-24 12:46:36 -0400

    Extra comment with proof

--- dictgen.py
+++ dictgen.py
@@ -22,6 +22,25 @@ You need to generate a dictionary according to the hint.
 2) The four-letter word is cyse, but each letter can be upper or lower case.
 
 3) You should be able to find the four digits on my personal website or ECE website. Note that there can be multiple possibilities of the four digits.
+
+[~] => aircrack-ng seven-01.cap -w /home/dhaynes/development/dictgen/out.txt
+
+      [00:26:53] 3993012/5406699 keys tested (2511.41 k/s)
+
+      Time left: 9 minutes, 22 seconds                          73.85%
+
+                         KEY FOUND! [ CySE2017@GMu) ]
+
+
+      Master Key     : 78 BC DE 0B 28 9E DA 47 E7 01 86 C2 83 0E 23 D8
+                       17 45 0E DF 50 39 75 DE 4D 01 AA E9 A7 18 15 F5
+
+      Transient Key  : 1C 91 9C DE 07 4F CB CB 5C FD 85 BC 7A 30 85 91
+                       C0 71 E6 48 B2 7C E5 30 74 63 FF A5 35 CE 73 73
+                       6C 31 88 A6 5D 79 FC 05 D2 B1 F1 34 58 B8 4C 16
+                       B0 5F 51 C2 1C 75 8D 5E F6 01 C2 F1 BC 5A 2F AA
+
+      EAPOL HMAC     : 3B A0 60 05 C7 A9 28 59 02 9F 38 E7 14 D5 21 A9
 """
 # Future Imports for py2 backwards compat.
 from __future__ import (absolute_import, division, print_function,
